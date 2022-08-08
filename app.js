3a8e9ce70a3d0a36003404039e363b9b73ffa689
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-08 13:47:33 +0530

    file system

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require("fs")
+// it is creating file or write text 
+// fs.writeFileSync('notes.txt','this  was created by node.js')
+ 
+// append data in existing file 
+fs.appendFileSync('notes.txt',  "i am priyanka")
