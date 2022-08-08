26f4d7275f6fce9e0a5a79643f7375f38dfb0924
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-08 15:16:04 +0530

    Chalk and validator module

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const fs = require("fs")
+// const fs = require("fs")
 // it is creating file or write text 
 // fs.writeFileSync('notes.txt','this  was created by node.js')
  
 // append data in existing file 
-fs.appendFileSync('notes.txt',  "i am priyanka")
+// fs.appendFileSync('notes.txt',  "i am priyanka")
+
+const add = require('./utils.js')
+const getNotes = require("./notes")
+const validator = require('validator')
+const chalk = require("chalk")
+const sum = add(4,-2)
+// console.log(sum);
+const notes = getNotes()
+console.log(chalk.bold.blue(notes));
+// console.log(validator.isEmail("priyanka@gmail.com"));
---
+++ utils.js
@@ -0,0 +1,5 @@
+const name = "Mike"
+const add = function(a,b){
+    return a+b
+}
+module.exports= add
